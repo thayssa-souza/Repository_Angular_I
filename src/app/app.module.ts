81d9e265d5e12e083e4c798097438605528db7d5
Thayssa Afonso Souza <105455780+thayssa-souza@users.noreply.github.com>
2022-12-06 22:57:27 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { AddressComponent } from './components/address/address.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    AddressComponent,
+    CarouselComponent,
+    ContactComponent,
+    FeaturesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
